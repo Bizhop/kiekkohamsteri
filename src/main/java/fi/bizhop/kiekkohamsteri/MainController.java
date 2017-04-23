8e83b7280949ae7479e1d89c8a1d67557893aa1d
Bizhop <ville.piispa@gmail.com>
2017-04-23 20:00:32 +0300

    logger

--- src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
@@ -1,5 +1,6 @@
 package fi.bizhop.kiekkohamsteri;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,8 @@ import fi.bizhop.kiekkohamsteri.model.R_vari;
 @Controller
 @RequestMapping(path="/hamsteri")
 public class MainController {
+	private static final Logger LOG = Logger.getLogger(MainController.class);
+	
 	@Autowired
 	private VariRepository variRepo;
 	
@@ -24,16 +27,19 @@ public class MainController {
 
 	@GetMapping(path="/varit")
 	public @ResponseBody Iterable<R_vari> haeVarit() {
+		LOG.debug("MainController.haeVarit()...");
 		return variRepo.findAll();
 	}
 	
 	@GetMapping(path="/kiekot")
 	public @ResponseBody Iterable<Kiekot> haeKiekot() {
+		LOG.debug("MainController.haeKiekot()...");
 		return kiekkoRepo.findAll();
 	}
 	
 	@RequestMapping(value="/kiekot/{id}", method=RequestMethod.GET)
 	public @ResponseBody Kiekot haeKiekko(@PathVariable Long id) {
+		LOG.debug(String.format("MainController.haeKiekko(%d)...", id));
 		return kiekkoRepo.findOne(id);
 	}
 }
