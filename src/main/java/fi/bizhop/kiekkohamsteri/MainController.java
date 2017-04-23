7bfe1f72b11bf67aa9d8c33e9080ca25a53b6b8a
Bizhop <ville.piispa@gmail.com>
2017-04-23 19:45:19 +0300

    yhden kiekon haku

--- src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
@@ -2,15 +2,16 @@ package fi.bizhop.kiekkohamsteri;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import fi.bizhop.kiekkohamsteri.model.Kiekot;
-import fi.bizhop.kiekkohamsteri.model.R_vari;
 import fi.bizhop.kiekkohamsteri.db.KiekkoRepository;
 import fi.bizhop.kiekkohamsteri.db.VariRepository;
+import fi.bizhop.kiekkohamsteri.model.Kiekot;
+import fi.bizhop.kiekkohamsteri.model.R_vari;
 
 @Controller
 @RequestMapping(path="/hamsteri")
@@ -29,5 +30,10 @@ public class MainController {
 	@GetMapping(path="/kiekot")
 	public @ResponseBody Iterable<Kiekot> haeKiekot() {
 		return kiekkoRepo.findAll();
+	}
+	
+	@RequestMapping(value="/kiekot/{id}", method=RequestMethod.GET)
+	public @ResponseBody Kiekot haeKiekko(@PathVariable Long id) {
+		return kiekkoRepo.findOne(id);
 	}
 }
