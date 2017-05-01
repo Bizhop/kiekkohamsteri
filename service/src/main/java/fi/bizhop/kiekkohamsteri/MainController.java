1460a4c86829fa39a464f1ac6cba36e0d58e58a3
Bizhop <ville.piispa@gmail.com>
2017-05-01 20:06:18 +0300

    loput crud operaaatiot

--- service/src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
+++ service/src/main/java/fi/bizhop/kiekkohamsteri/MainController.java
@@ -3,8 +3,12 @@ package fi.bizhop.kiekkohamsteri;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -37,9 +41,45 @@ public class MainController {
 		return kiekkoRepo.findAll();
 	}
 	
-	@RequestMapping(value="/kiekot/{id}", method=RequestMethod.GET)
+	@GetMapping(value="/kiekot/{id}")
 	public @ResponseBody Kiekot haeKiekko(@PathVariable Long id) {
 		LOG.debug(String.format("MainController.haeKiekko(%d)...", id));
 		return kiekkoRepo.findOne(id);
+	}
+	
+	@PostMapping(value="/kiekot")
+	public @ResponseBody Kiekot lisaaKiekko() {
+		LOG.debug("MainController.lisaaKiekko()...");
+		
+		Kiekot kiekko = new Kiekot();
+		kiekko = kiekkoRepo.save(kiekko);
+		
+		LOG.debug(String.format("Lisätty kiekko, id=%d", kiekko.getId()));
+		
+		return kiekko;
+	}
+	
+	@PutMapping(value="/kiekot/{id}")
+	public @ResponseBody Kiekot muutaKiekkoa(@RequestBody Kiekot kiekko) {
+		LOG.debug(String.format("MainController.muutaKiekkoa()... id=%d", kiekko.getId()));
+		
+		kiekko = kiekkoRepo.save(kiekko);
+		
+		LOG.debug(String.format("Muutettu kiekkoa, id=%s", kiekko.getId()));
+		
+		return kiekko;
+	}
+	
+	@DeleteMapping(value="/kiekot/{id}")
+	public @ResponseBody boolean poistaKiekko(@PathVariable Long id) {
+		LOG.debug(String.format("MainController.poistaKiekko(%d)...", id));
+		
+		if(kiekkoRepo.exists(id)) {
+			kiekkoRepo.delete(id);
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
