6e9e20649037c58f9c360169bb0ad18f1f3a0e22
Ville Piispa <ville.piispa@gmail.com>
2022-08-22 13:56:03 +0300

    quick import fix

--- src/main/java/fi/bizhop/kiekkohamsteri/WebConfiguration.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/WebConfiguration.java
@@ -1,12 +1,10 @@
 package fi.bizhop.kiekkohamsteri;
 
 import fi.bizhop.kiekkohamsteri.service.AuthService;
-import liquibase.pro.packaged.O;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
