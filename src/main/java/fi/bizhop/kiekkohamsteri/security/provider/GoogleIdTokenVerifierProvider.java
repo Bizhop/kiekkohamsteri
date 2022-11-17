56590ffa78b1df8b2fa9175af2e3e5c574c482cb
Ville Piispa <ville.piispa@gmail.com>
2022-11-17 13:15:58 +0200

    support for multiple google audiences

--- src/main/java/fi/bizhop/kiekkohamsteri/security/provider/GoogleIdTokenVerifierProvider.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/security/provider/GoogleIdTokenVerifierProvider.java
@@ -5,13 +5,22 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 public class GoogleIdTokenVerifierProvider {
     public GoogleIdTokenVerifier getVerifier() {
+        var audiencesEnv = System.getenv("HAMSTERI_GOOGLE_AUDIENCES");
+        var audiences = Arrays.stream(audiencesEnv.split(","))
+                .map(prefix -> prefix + ".apps.googleusercontent.com")
+                .collect(Collectors.toList());
+
         return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(List.of(System.getenv("HAMSTERI_GOOGLE_CLIENT_ID")))
+                .setAudience(audiences)
                 .build();
     }
 }
