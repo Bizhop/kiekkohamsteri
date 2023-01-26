a50ad59255ce1a35dca099c0aa9417bdae0bb497
Ville Piispa <ville.piispa@gmail.com>
2023-01-26 11:05:13 +0200

    Clear group requests when deleting group (#31)

--- src/main/java/fi/bizhop/kiekkohamsteri/db/GroupRepository.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/db/GroupRepository.java
@@ -1,10 +1,17 @@
 package fi.bizhop.kiekkohamsteri.db;
 
 import fi.bizhop.kiekkohamsteri.model.Group;
+import fi.bizhop.kiekkohamsteri.model.GroupRequest;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
+import java.util.Set;
 
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    @Override
+    @NonNull
     List<Group> findAll();
+
+    List<Group> findAllByIdIn(Set<Long> ids);
 }
--- src/main/java/fi/bizhop/kiekkohamsteri/db/GroupRequestRepository.java
+++ src/main/java/fi/bizhop/kiekkohamsteri/db/GroupRequestRepository.java
@@ -6,8 +6,10 @@ import fi.bizhop.kiekkohamsteri.model.GroupRequest.Status;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Set;
 
 public interface GroupRequestRepository extends CrudRepository<GroupRequest, Long> {
     List<GroupRequest> findByStatus(Status status);
     List<GroupRequest> findAllByGroupInAndStatus(Iterable<Group> groups, Status status);
+    List<GroupRequest> findAllByGroup(Group group);
 }
